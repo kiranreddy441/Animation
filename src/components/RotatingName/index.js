654ca6593444a9994ddf0e91d38e5d982e4e9e1f
kiranreddy441 <jyothikiranreddychalla123@gmail.com>
2023-04-18 15:36:08 +0530

    animation

---
+++ src/components/RotatingName/index.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+import './index.css'
+
+class RotatingName extends Component{
+    render(){
+        return(
+            <div className="app-container">
+                <div className="box">
+                    <p>kiran reddy</p>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default RotatingName
